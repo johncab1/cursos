8bbdaa938f2a6a8add1fc9d227acc7c61811b75f
johncab1 <jonatan.orc@gmail.com>
2022-06-01 21:44:26 -0500

    se agregan callbacks a modelo video

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,22 @@ class Video < ApplicationRecord
 
   validate :validate_custom_title
 
+  before_save :send_notification
+  after_save :send_mail, if: :has_description?
+
+  private
+  def send_notification
+    puts "Nuevo video!!"
+  end
+
+  def send_mail
+    puts "Enviar correo!!"
+  end
+
+  def has_description?
+    !description.nil? && !description.empty?
+  end
+
   def validate_custom_title
       errors.add(:title, "No es posible usar ese titulo") if title == 'CodigoFacilito'
   end
