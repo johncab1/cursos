0861cabf826e5ef7e469a2eec4493149e28c06f9
johncab1 <jonatan.orc@gmail.com>
2022-06-05 22:48:07 -0500

    uso de joins

--- app/models/course.rb
+++ app/models/course.rb
@@ -14,4 +14,15 @@ class Course < ApplicationRecord
   has_many :videos
   has_many :categories, through: :videos, source: :categories
   has_many :comments, as: :commentable
+
+  def self.has_videos
+    #joins(:videos).where(videos: {visible: true})
+    joins("inner join videos on courses.id = videos.course_id")
+      .where(videos: {visible: true})
+  end
+
+  def self.no_has_videos
+    joins("left join videos on courses.id = videos.course_id")
+      .where("videos.id is null")
+  end
 end
